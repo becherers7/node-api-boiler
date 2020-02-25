be0f89ef8cceef0d2edde2193f226b3612095eb4
Steve Becherer <becherers7@gmail.com>
2020-02-24 21:08:46 -0600

    Update mySqlConfig.js

--- config/mySqlConfig.js
+++ config/mySqlConfig.js
@@ -2,7 +2,7 @@ let mysql = require("mysql");
 exports.connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Gre.Xsb.312",
+  password: "",
   port: 3306,
   database: "node_api",
   multipleStatements: true
