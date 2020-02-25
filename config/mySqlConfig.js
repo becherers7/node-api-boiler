f9d02ae37e8c17f70793bb5c1cc79bae1197489b
Steve Becherer <becherers7@gmail.com>
2020-02-24 21:21:11 -0600

    delete mySqlConfig.js

--- config/mySqlConfig.js
+++
@@ -1,9 +0,0 @@
-let mysql = require("mysql");
-exports.connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "node_api",
-  multipleStatements: true
-});
